11639d685936c8e329fa3e25192bc40ec4261ed9
origamium <onoueorigami@gmail.com>
2020-04-06 01:48:08 +0900

    feat: 栃木県への文言の変更

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -14,7 +14,7 @@ const config: Configuration = {
     htmlAttrs: {
       prefix: 'og: http://ogp.me/ns#'
     },
-    titleTemplate: '%s | 東京都 新型コロナウイルス感染症対策サイト',
+    titleTemplate: '%s | 栃木県 新型コロナウイルス感染症非公式対策サイト',
     meta: [
       { charset: 'utf-8' },
       { name: 'viewport', content: 'width=device-width, initial-scale=1' },
@@ -22,7 +22,7 @@ const config: Configuration = {
       {
         hid: 'og:url',
         property: 'og:url',
-        content: 'https://stopcovid19.metro.tokyo.lg.jp'
+        content: 'https://covid-19-tochigi.origamium.net/'
       },
       {
         hid: 'twitter:card',
@@ -32,12 +32,7 @@ const config: Configuration = {
       {
         hid: 'twitter:site',
         name: 'twitter:site',
-        content: '@tokyo_bousai'
-      },
-      {
-        hid: 'twitter:creator',
-        name: 'twitter:creator',
-        content: '@tokyo_bousai'
+        content: '@pref_tochigi'
       },
       {
         hid: 'fb:app_id',
@@ -157,7 +152,7 @@ const config: Configuration = {
     // hardSource: process.env.NODE_ENV === 'development'
   },
   manifest: {
-    name: '東京都 新型コロナウイルス感染症対策サイト',
+    name: '栃木県 新型コロナウイルス感染症非公式対策サイト',
     theme_color: '#00a040',
     background_color: '#ffffff',
     display: 'standalone',
